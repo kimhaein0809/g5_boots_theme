f6122656fe34a5a5c7703838212c0a8abaec15de
kimhaein0809 <laura0809@naver.com>
2022-09-15 16:25:32 +0900

    fdf

--- boot/index.php
+++ boot/index.php
@@ -198,18 +198,131 @@ include_once(G5_THEME_PATH.'/head.php');
 </div>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<div class="container position-relative slide">
-    <h2 class="text-center">갤러리</h2>
-    <img src="<?php echo G5_THEME_IMG_URL?>/pre.png" alt="" class="prev d-none d-md-block" >
-    <img src="<?php echo G5_THEME_IMG_URL?>/next.png" alt="" class="next d-none d-md-block">
-    <div class="responsive">
-    <?php
-            echo latest('theme/pic_haein', 'gallery', 7, 23);	
-        ?>
+<div class="slideWrap position-relative">
+    <div class="container position-relative slide">
+        <h2 class="text-center">갤러리</h2>
+        <img src="<?php echo G5_THEME_IMG_URL?>/pre.png" alt="" class="prev d-none d-md-block" >
+        <img src="<?php echo G5_THEME_IMG_URL?>/next.png" alt="" class="next d-none d-md-block">
+        <div class="responsive">
+        <?php
+                echo latest('theme/pic_haein', 'gallery', 7, 23);
+            ?>
+        </div>
     </div>
 </div>
 <div class="cont7">
-    
+        <div class="container cont77">
+            <h2 class="text-center title mb-5">체험 수업 후기</h2>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card text-center text-md-start">
+                        <img class="card-img-top mb-2" src="<?php echo G5_THEME_IMG_URL?>/main_review01.png" alt="Title">
+                        <div class="card-body pb-5">
+                            <h4 class="card-title">저희 아이에게 아주 딱 맞는 수업</h4>
+                            <p class="card-text">
+                                아이 친구네와 함께 체험 수업을 신청했는데,
+                                너무 재미있어 합니다.
+                                수업이 끝난 후 또 하고 싶다고 해서 정규 수업 등록을 고민하고 있어요~~
+                                선생님께서 아이의 수준에 맞춰 수업을 진행해주시고
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card text-center text-md-start">
+                        <img class="card-img-top mb-2" src="<?php echo G5_THEME_IMG_URL?>/main_review02.png" alt="Title">
+                        <div class="card-body pb-5">
+                            <h4 class="card-title">레몬 추천합니다!!!:)</h4>
+                            <p class="card-text">
+                            저희 아이는 온라인 수업을 자주 듣곤 하는데, 생각했
+                                던 일반 화상 수업과 달리 기능이 너무 다양하고 정교
+                                해서 정말 놀랐어요~!! 수업 내용도 흥미로웠고, 강사
+                                분들의 실력도 믿음직스러웠습니다. 만족해요~~
+                                레몬 추천합니다!!! :)
+            
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card text-center text-md-start">
+                        <img class="card-img-top mb-2" src="<?php echo G5_THEME_IMG_URL?>/main_review03.png" alt="Title">
+                        <div class="card-body pb-5">
+                            <h4 class="card-title">영어가 좋아졌대요.</h4>
+                            <p class="card-text">
+                                영어 수업도 처음이고 온라인 수업도 처음인 저희 아
+                                이가 체험 수업을 듣고 영어가 좋아졌대요. 선생님들
+                                도 좋고, 수업 내내 너무 재미있었다고 합니다~ 걱정
+                                했는데 아이가 이렇게 좋아할 줄은 몰랐습니다. 레몬
+                                감사해요^-^
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+<div class="footer">
+    <div class="top container">
+        <div class="row">
+            <div class="col-md-2">
+                <h4><a href="#">YBM LEMON 스토리</a></h4>
+                <p><a href="#">상담신청</a></p>
+            </div>
+            <div class="col-md-2">
+                <h4><a href="#">수강신청</a></h4>
+                <ul>
+                    <li><a href="#">수강신청</a></li>
+                    <li><a href="#">무료체험</a></li>
+                    <li><a href="#">레벨테스트</a></li>
+                    <li><a href="#">무료체험&레벨테스트 신청확인</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <h4><a href="#">커뮤니티</a></h4>
+                <ul>
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">Lemon Seed Mall</a></li>
+                    <li><a href="#">이벤트</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <h4><a href="#">마이클래스</a></h4>
+                <ul>
+                    <li><a href="#">Calendar</a></li>
+                    <li><a href="#">Report</a></li>
+                    <li><a href="#">숙제 제출 및 확인</a></li>
+                    <li><a href="#">Lemon Seed</a></li>
+                    <li><a href="#">수강현황</a></li>
+                    <li><a href="#">정회원 공지사항</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <h4><a href="#">My Lemon</a></h4>
+                <ul>
+                    <li><a href="#">수강신청 내역</a></li>
+                    <li><a href="#">포인트몰 구매내역</a></li>
+                    <li><a href="#">학부모정보</a></li>
+                    <li><a href="#">학습자정보</a></li>
+                    <li><a href="#">1:1 답변확인</a></li>
+                    <li><a href="#">학습장애답변확인</a></li>
+                    <li><a href="#">상담신청내역</a></li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <h4><a href="#">고객센터</a></h4>
+                <ul>
+                    <li><a href="#">공지사항</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">1:1 상담</a></li>
+                    <li><a href="#">학습장애신고</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="btm container">
+
+    </div>
 </div>
 
 
